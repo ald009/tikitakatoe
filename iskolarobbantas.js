057284f75ad55a76a862051e8f8a9e48f1767464
Kovács Tibor <kovacs.tibor@diak.szbi-pg.hu>
2026-02-05 10:44:28 +0100

    d

--- iskolarobbantas.js
+++ iskolarobbantas.js
@@ -4,6 +4,22 @@ let tabla = [
     ["", "", ""]
 ]
 let akt = "X";
+
+function win(){
+    if(tabla[0][0] != "" && tabla[0][0] == tabla[0][1] && tabla[0][1] == tabla[0][2]) return true;
+    if(tabla[1][0] != "" && tabla[1][0] == tabla[1][1] && tabla[1][1] == tabla[1][2]) return true;
+    if(tabla[2][0] != "" && tabla[2][0] == tabla[2][1] && tabla[2][1] == tabla[2][2]) return true; //sorok
+
+    if(tabla[0][0] != "" && tabla[0][0] == tabla[1][0] && tabla[1][0] == tabla[2][0]) return true;
+    if(tabla[0][1] != "" && tabla[0][1] == tabla[1][1] && tabla[1][1] == tabla[2][1]) return true;
+    if(tabla[0][2] != "" && tabla[0][2] == tabla[1][2] && tabla[1][2] == tabla[2][2]) return true; //oszlopok
+
+    if(tabla[0][0] != "" && tabla[0][0] == tabla[1][1] && tabla[1][1] == tabla[2][2]) return true;
+    if(tabla[0][2] != "" && tabla[0][2] == tabla[1][1] && tabla[1][1] == tabla[2][0]) return true; //atlos
+
+    return false;
+}
+
 function katt(id) {
     let sor = parseInt(id[0]);
     let oszlop = parseInt(id[1]);
@@ -12,11 +28,20 @@ function katt(id) {
     if (tabla[sor][oszlop] == "") {
         tabla[sor][oszlop] = akt;
         document.getElementById("n" + id).innerHTML = akt;
-        if (akt == "X") {
-            akt = "O";
-        } else {
-            akt = "X";
+
+        if(win()){
+            window.alert(akt + " nyert!");
+            document.getElementById("buttonka").click();
         }
+        else {
+
+            if (akt == "X") {
+                akt = "O";
+            } else {
+                akt = "X";
+            }
+        }
+
     }
 }
 
